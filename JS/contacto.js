6ded9d82c10b3c05c3f500a9ab2f9014aeb5496a
leandro peluffo <leanpelu324@gmail.com>
2023-02-06 17:30:44 -0300

    ajustes en formulario

--- JS/contacto.js
+++ JS/contacto.js
@@ -9,15 +9,28 @@ const errorNodes = document.querySelector(".error");
 function validarForm(){
     
     vaciarMensajes();
+    let errorFlag = false;
 
     if(nombre.Value.length < 2){
         errorNodes[0].innerText = "El nombre debe ser mas largo";
         nombre.classList.add("error-border");
+        errorFlag = true;
     }
 
     if(!emailValido(email.Value)){
         errorNodes[1].innerText = "Mail invalido";
-        nombre.classList.add("error-border")
+        nombre.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(mensaje.value.length < 1){
+        errorNodes[2].innerText = "por favor ingrese un mensaje";
+        mensaje.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!errorFlag){
+        success.innerText = "enviado"
     }
 }
 
@@ -26,8 +39,10 @@ function vaciarMensajes(){
     for(let i = 0; i < errorNodes.length; i++){
         errorNodes[i].innerText = "";
     }
+    success.innerText = "";
     nombre.classList.remove("error-border");
     email.classList.remove("error-border");
+    mensaje.classList.remove("error-border");
 }
 
 
